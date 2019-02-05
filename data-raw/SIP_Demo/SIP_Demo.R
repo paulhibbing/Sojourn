a5bcdd8787002fbf8e8a8216fa792c81d4a26a6a
Paul R. Hibbing <paulhibbing@gmail.com>
2019-02-04 23:58:33 -0500

    Remove AP file reading baggage and run R CMD Check

--- data-raw/SIP_Demo/SIP_Demo.R
+++ data-raw/SIP_Demo/SIP_Demo.R
@@ -1,5 +1,9 @@
 rm(list = ls())
 devtools::load_all()
+
+
+# IGNORE THIS SECTION -----------------------------------------------------
+
 # source("data-raw/Lyden_Demo/sojourn.functions.R")
 # load("data/example_data.rda")
 
@@ -8,27 +12,40 @@ devtools::load_all()
 #   "data-raw/SIP_Demo/sampledata_1secDataTable.csv",
 #   skip = 11
 # )
-ag <- AGread::read_AG_counts(
-  "data-raw/SIP_Demo/sampledata_1secDataTable.csv",
-  skip = 11
-)
 
-ag <- setNames(
-  with(
-    ag,
-    data.frame(
-      Axis1, Axis2, Axis3, Vector.Magnitude, #as.character(Timestamp),
-      Timestamp,
-      stringsAsFactors = FALSE
-    )
-  ),
-  c("counts", "axis2", "axis3", "vm", "Time")
-)
+# RE-RUN THIS SECTION IF NEEDED -------------------------------------------
+
+# ag <- AGread::read_AG_counts(
+#   "data-raw/SIP_Demo/sampledata_1secDataTable.csv",
+#   skip = 11
+# )
+#
+# ag <- setNames(
+#   with(
+#     ag,
+#     data.frame(
+#       Axis1, Axis2, Axis3, Vector.Magnitude, #as.character(Timestamp),
+#       Timestamp,
+#       stringsAsFactors = FALSE
+#     )
+#   ),
+#   c("counts", "axis2", "axis3", "vm", "Time")
+# )
+#
+# SIP_ag <- ag
+# devtools::use_data(SIP_ag, overwrite = TRUE)
+# rm(SIP_ag)
 
 # leaving out these two steps *should* give the same output as plain
 # sojourns; if it doesn't, please report it as a bug
-ap <- AP.file.reader("data-raw/SIP_Demo/sampledata Events.csv")
-ag <- enhance.actigraph(ag, ap)
+
+load("data/SIP_ag.rda")
+
+# SIP_ap <- AP.file.reader("inst/extdata/sampledata Events.csv")
+# devtools::use_data(SIP_ap, overwrite = TRUE)
+load("data/SIP_ap.rda")
+
+ag <- enhance.actigraph(SIP_ag, SIP_ap)
 
 # run SIP
 sip.estimate <- sojourn_3x_SIP(ag)
